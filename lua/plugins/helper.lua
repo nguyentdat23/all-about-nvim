234a4ec170c205770b527058d197ab8ae0e335d3
Dat Nguyen <dat.nt31@shbfinance.com.vn>
2024-11-04 20:41:13 +0700

    Blink.cmp draw function show lib source

--- lua/plugins/helper.lua
+++ lua/plugins/helper.lua
@@ -15,9 +15,39 @@ return {
     -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
     -- build = 'cargo build --release',
 
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
     opts = {
       keymap = {
         preset = "enter",
+      },
+      windows = {
+        autocomplete = {
+          draw = function(ctx)
+            local icon, cmp_item = ctx.kind_icon, ctx.item
+            local cmp_source = ""
+
+            if cmp_item.labelDetails and cmp_item.labelDetails.description then
+              cmp_source = cmp_item.labelDetails.description
+            end
+
+            return {
+              {
+                " " .. ctx.item.label,
+                fill = true,
+                -- hl_group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
+              },
+              {
+                string.format(" %s %-10s", icon, ctx.kind),
+                hl_group = "BlinkCmpKind" .. ctx.kind,
+              },
+              {
+                string.format("%-2s", cmp_source),
+                hl_group = "BlinkCmpSource",
+              },
+            }
+          end,
+        },
       },
       highlight = {
         use_nvim_cmp_as_default = true,
