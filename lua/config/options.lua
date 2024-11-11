559793e6d14a3fe9f9d4d0eb8bb476c711ea61cb
Dat Nguyen <nguyentdat23@gmail.com>
2024-11-11 22:18:20 +0700

    Set variables for dynamic theming using lumen-nvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -30,3 +30,7 @@ vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true }
 vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+
+-- Set theme light/dark using lumen-nvim
+vim.g.lumen_light_colorscheme = "edge"
+vim.g.lumen_dark_colorscheme = "catppuccin-frappe"
