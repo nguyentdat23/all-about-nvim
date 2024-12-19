dfc86701838de1a30d4e5a0924bf88be6625396a
Dat Nguyen Thanh <dat.nt31@shbfinance.com.vn>
2024-12-19 09:23:54 +0700

    Remove deprecated plugin

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,23 +1,20 @@
 return {
-  { "hrsh7th/nvim-cmp", enabled = false },
+  {
+    "ggandor/leap.nvim",
+  },
   {
     "saghen/blink.cmp",
-    lazy = false, -- lazy loading handled internally
-    -- optional: provides snippets for the snippet source
-    dependencies = { "rafamadriz/friendly-snippets", "mikavilpas/blink-ripgrep.nvim" },
-
-    -- use a release tag to download pre-built binaries
-    version = "v0.*",
 
     ---@module 'blink.cmp'
-    --- @type blink.cmp.Config
+    ---@type blink.cmp.Config
     opts = {
-      keymap = { preset = "enter" },
       appearance = {
         use_nvim_cmp_as_default = true,
-        nerd_font_variant = "mono",
       },
       completion = {
+        trigger = {
+          show_on_insert_on_trigger_character = false,
+        },
         menu = {
           draw = {
             components = {
@@ -38,53 +35,18 @@ return {
                 end,
                 highlight = "BlinkCmpLabelDescription",
               },
-            },
-          },
-        },
-        list = {
-          max_items = 50,
-        },
-      },
 
-      -- default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, via `opts_extend`
-      sources = {
-        completion = {
-          enabled_providers = { "lsp", "snippets" },
-        },
-        providers = {
-          ripgrep = {
-            module = "blink-ripgrep",
-            name = "Ripgrep",
-            -- the options below are optional, some default values are shown
-            ---@module "blink-ripgrep"
-            ---@type blink-ripgrep.Options
-            opts = {
-              -- For many options, see `rg --help` for an exact description of
-              -- the values that ripgrep expects.
-
-              -- the minimum length of the current word to start searching
-              -- (if the word is shorter than this, the search will not start)
-              prefix_min_len = 3,
-
-              -- The number of lines to show around each match in the preview window
-              context_size = 3,
-
-              -- The maximum file size that ripgrep should include in its search.
-              -- Useful when your project contains large files that might cause
-              -- performance issues.
-              -- Examples: "1024" (bytes by default), "200K", "1M", "1G"
-              max_filesize = "1M",
+              source_name = {
+                width = { max = 30 },
+                text = function(ctx)
+                  return ctx.source_name
+                end,
+                highlight = "BlinkCmpSource",
+              },
             },
           },
         },
       },
     },
-    -- allows extending the enabled_providers array elsewhere in your config
-    -- without having to redefine it
-    opts_extend = { "sources.completion.enabled_providers" },
-  },
-  {
-    "ggandor/leap.nvim",
   },
 }
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -15,56 +15,17 @@ return {
       },
     },
   },
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {
-      settings = {
-        expose_as_code_action = "all",
-      },
-    },
-  },
+  -- {
+  --   "pmizio/typescript-tools.nvim",
+  --   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  -- },
   {
     "neovim/nvim-lspconfig",
     dependencies = { "saghen/blink.cmp" },
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- tsserver will be automatically installed with mason and loaded with lspconfig
-        vtsls = {
-          enabled = false,
-        },
-      },
-      config = function(_, opts)
-        -- config blink.cmp to work better with nvim-lspconfig
-        local lspconfig = require("lspconfig")
-        for server, config in pairs(opts.servers or {}) do
-          config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-          lspconfig[server].setup(config)
-        end
-      end,
-    },
   },
   {
     "folke/trouble.nvim",
-    opts = {
-      modes = {
-        preview_float = {
-          mode = "lsp",
-          preview = {
-            type = "float",
-            relative = "editor",
-            border = "rounded",
-            title = "Preview",
-            title_pos = "center",
-            position = { 0, -2 },
-            size = { width = 0.3, height = 0.3 },
-            zindex = 200,
-          },
-        },
-      },
-    }, -- for default options, refer to the configuration section for custom setup.
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
     cmd = "Trouble",
   },
 }
